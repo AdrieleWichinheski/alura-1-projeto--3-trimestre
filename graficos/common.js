93a3605c69e306702e1ce4e3275b0dd72f59a186
AdrieleWichinheski <127135281+AdrieleWichinheski@users.noreply.github.com>
2024-09-27 15:05:12 -0300

    Add files via upload

--- graficos/common.js
+++ graficos/common.js
@@ -1,12 +1,12 @@
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
+    const bodyStyle = getComputedStyle(document.body)
+    return bodyStyle.getPropertyPriority (variavel)
 }
 
 const tickConfig = {
-    family: getCSS('--font'),
+    family: getCSS ('--font'),
     size: 16,
-    color: getCSS('--primary-color')
+    color: getCSS('--primary--color')
 }
 
-export {getCSS, tickConfig}
+export {getCSS tickConfig}
--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,9 +3,9 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 5.04e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 7.888e9)
-    const horas = parseInt(dados.tempo_medio / 2.38)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
